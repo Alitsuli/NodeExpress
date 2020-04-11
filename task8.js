bb57eedd06f79b0cf75941a62a87aebf550c1d23
altsuli <ali_reza222@hotmail.com>
2020-04-11 19:32:57 +0300

    8 tasks complete

---
+++ task8.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var url = require('url');
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "olso",
+    password: "olso",
+    database: "example_db"
+});
+
+app.get("/api/location", function (req, res) {
+    console.log("get location details");
+    var q = url.parse(req.url, true).query;
+    var nimi = q.name;
+    var sql = "SELECT * from location WHERE Location_name = ?";
+    con.query(sql, [nimi], function (err, result){
+         if (err)
+             throw (err);
+         else
+             console.log(result);
+             res.end(JSON.stringify(result));
+    })
+});
+
+var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Example app listening at http://%s:%s", host, port)
+});
